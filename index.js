db9c7134fd992938216fdb60781fbb978ad0474d
Billy Kang <billy@nurigo.net>
2017-10-24 10:42:22 +0900

    modify index.js

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
 
+
+app.get('/', function(req, res) {
+  res.send('root is called from testapp')
+});
+
 app.get('/user', function (req, res) {
-  res.send('Hello User! Version 3');
+  res.send('Hello User! Version 3 -testapp');
 });
 
 app.get('/healthcheck', function (req, res) {
